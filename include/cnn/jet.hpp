ddd63c601729418487f6ec7eb3945d084016c3a5
Kuang Fangjun <csukuangfj@gmail.com>
2018-11-14 20:55:31 +0800

    support polynomial autodiff.
    supported operations: +, -, *, /

--- include/cnn/jet.hpp
+++ include/cnn/jet.hpp
@@ -68,6 +68,14 @@ ArrayWithOp<Dtype, N> operator*(double s, const ArrayWithOp<Dtype, N>& a)
     return c;
 }
 
+template<typename Dtype, int N>
+ArrayWithOp<Dtype, N> operator/(const ArrayWithOp<Dtype, N>& a, double s)
+{
+    ArrayWithOp<Dtype, N> c;
+    for (int i = 0; i < N; i++) c[i] = a[i]/s;
+    return c;
+}
+
 
 /**
  * We use the same name `Jet` as in ceres-solver.
@@ -88,11 +96,11 @@ class Jet
         v_.fill(0);
     }
 
-    Jet(const Dtype& a, int i)
+    Jet(const Dtype& a, int i, Dtype value = 1)
     {
         a_ = a;
         v_.fill(0);
-        v_.at(i) = Dtype(1);
+        v_.at(i) = Dtype(value);
     }
 
     std::string to_string() const
@@ -115,7 +123,6 @@ class Jet
         *this = *this - f;
         return *this;
     }
-#if 0
     Jet& operator *=(const Jet& f)
     {
         *this = *this * f;
@@ -126,7 +133,6 @@ class Jet
         *this = *this / f;
         return *this;
     }
-#endif
 
     Dtype a_;                   //!< value
     ArrayWithOp<Dtype, N> v_;   //!< gradient
@@ -192,7 +198,39 @@ Jet<Dtype, N> operator - (Dtype s, const Jet<Dtype, N>& f)
 }
 
 template<typename Dtype, int N>
-Jet<Dtype, N> operator+(const Jet<Dtype, N>& f, const Jet<Dtype, N>& g)
+Jet<Dtype, N> operator * (const Jet<Dtype, N>& f, Dtype s)
+{
+    Jet<Dtype, N> res;
+    res.a_ = f.a_ * s;
+    res.v_ = f.v_ * s;
+}
+
+template<typename Dtype, int N>
+Jet<Dtype, N> operator * (Dtype s, const Jet<Dtype, N>& f)
+{
+    Jet<Dtype, N> res;
+    res.a_ = f.a_ * s;
+    res.v_ = f.v_ * s;
+}
+
+template<typename Dtype, int N>
+Jet<Dtype, N> operator / (const Jet<Dtype, N>& f, Dtype s)
+{
+    Jet<Dtype, N> res;
+    res.a_ = f.a_ / s;
+    res.v_ = f.v_ / s;
+}
+
+template<typename Dtype, int N>
+Jet<Dtype, N> operator / (Dtype s, const Jet<Dtype, N>& f)
+{
+    Jet<Dtype, N> res;
+    res.a_ = s / f.a_;
+    res.v_ = - s * f.v / (f.a_ * f.a_);
+}
+
+template<typename Dtype, int N>
+Jet<Dtype, N> operator + (const Jet<Dtype, N>& f, const Jet<Dtype, N>& g)
 {
     Jet<Dtype, N> res;
     res.a_ = f.a_ + g.a_;
@@ -201,11 +239,29 @@ Jet<Dtype, N> operator+(const Jet<Dtype, N>& f, const Jet<Dtype, N>& g)
 }
 
 template<typename Dtype, int N>
-Jet<Dtype, N> operator-(const Jet<Dtype, N>& f, const Jet<Dtype, N>& g)
+Jet<Dtype, N> operator - (const Jet<Dtype, N>& f, const Jet<Dtype, N>& g)
 {
     Jet<Dtype, N> res;
     res.a_ = f.a_ - g.a_;
     res.v_ = f.v_ - g.v_;
+    return res;
+}
+
+template<typename Dtype, int N>
+Jet<Dtype, N> operator * (const Jet<Dtype, N>& f, const Jet<Dtype, N>& g)
+{
+    Jet<Dtype, N> res;
+    res.a_ = f.a_ * g.a_;
+    res.v_ = f.a_*g.v_ + g.a_*f.v_;
+    return res;
+}
+
+template<typename Dtype, int N>
+Jet<Dtype, N> operator / (const Jet<Dtype, N>& f, const Jet<Dtype, N>& g)
+{
+    Jet<Dtype, N> res;
+    res.a_ = f.a_ / g.a_;
+    res.v_ = f.v_ / g.a_ - (f.a_/(g.a_*g.a_))*g.v_;
     return res;
 }
 
--- test/test_jet.cpp
+++ test/test_jet.cpp
@@ -15,23 +15,82 @@ class JetTest : public ::testing::Test
 using MyTypes = ::testing::Types<float, double>;
 TYPED_TEST_CASE(JetTest, MyTypes);
 
+// x = x + 1
 template<typename Dtype>
 Dtype add_scalar1(const Dtype& x) {Dtype y; y = x + Dtype(1); return y;}
 
+// x = 1 + x
 template<typename Dtype>
 Dtype add_scalar2(const Dtype& x) {Dtype y; y = Dtype(1) + x; return y;}
 
+// x += 1
 template<typename Dtype>
 Dtype add_scalar3(const Dtype& x) {Dtype y(x); y += Dtype(1); return y;}
 
+// x = x - 1
 template<typename Dtype>
 Dtype sub_scalar1(const Dtype& x) {Dtype y; y = x - Dtype(1); return y;}
 
+// x = 1 - x
 template<typename Dtype>
 Dtype sub_scalar2(const Dtype& x) {Dtype y; y = Dtype(1) - x; return y;}
 
+// x -= 1
 template<typename Dtype>
 Dtype sub_scalar3(const Dtype& x) {Dtype y(x); y -= Dtype(1); return y;}
+
+// x = x * 2
+template<typename Dtype>
+Dtype mul_scalar1(const Dtype& x) {Dtype y; y = x * Dtype(2); return y;}
+
+// x = 2 * x
+template<typename Dtype>
+Dtype mul_scalar2(const Dtype& x) {Dtype y; y = Dtype(2) * x; return y;}
+
+// x *= 2
+template<typename Dtype>
+Dtype mul_scalar3(const Dtype& x) {Dtype y(x); y *= Dtype(2); return y;}
+
+// x = x / 2
+template<typename Dtype>
+Dtype div_scalar1(const Dtype& x) {Dtype y; y = x / Dtype(2); return y;}
+
+// x = 2 / x
+template<typename Dtype>
+Dtype div_scalar2(const Dtype& x) {Dtype y; y = Dtype(2) / x; return y;}
+
+// x /= 2
+template<typename Dtype>
+Dtype div_scalar3(const Dtype& x) {Dtype y(x); y /= Dtype(2); return y;}
+
+
+template<typename Dtype>
+Dtype jet_mul_div_add(const Dtype& x, const Dtype& y)
+{
+    Dtype z;
+    z = x*y/(x*x + y);
+    return z;
+    // from https://www.whitman.edu/mathematics/calculus_online/section14.03.html
+    /*
+     * dz/dx = (y*y - x*x*y)/((x*x+y)*(x*x+y))
+     * dz/dy = x*x*x/((x*x+y)*(x*x+y))
+     */
+}
+
+template<typename Dtype>
+Dtype jet_sub_div(const Dtype& x, const Dtype& y)
+{
+    Dtype z;
+    z = (x - y)/(x + y);
+    return z;
+    // http://math.gmu.edu/~memelian/teaching/Fall08/partDerivExamples.pdf
+    // 1 (e)
+    /*
+     * dz/dx = 2y/((x+y)*(x+y))
+     * dz/dy = -2x/((x+y)*(x+y))
+     */
+}
+
 
 
 TYPED_TEST(JetTest, scalar_add)
@@ -82,6 +141,82 @@ TYPED_TEST(JetTest, negate)
     f = -f;
     EXPECT_EQ(f.a_, -10);
     EXPECT_EQ(f.v_[0], -1);
+}
+
+TYPED_TEST(JetTest, scalar_mul)
+{
+    Jet<TypeParam, 1> f(10, 0);
+
+    // x = x * 2
+    f = mul_scalar1(f);
+    EXPECT_EQ(f.a_, 20);
+    EXPECT_EQ(f.v_[0], 2);
+
+    // x = 2 * x
+    f = mul_scalar2(f);
+    EXPECT_EQ(f.a_, 40);
+    EXPECT_EQ(f.v_[0], 4);
+
+    // x *= 2
+    f = mul_scalar3(f);
+    EXPECT_EQ(f.a_, 80);
+    EXPECT_EQ(f.v_[0], 8);
+}
+
+TYPED_TEST(JetTest, scalar_div)
+{
+    Jet<TypeParam, 1> f(10, 0);
+
+    // x = x / 2
+    f = div_scalar1(f);
+    EXPECT_EQ(f.a_, 5);
+    EXPECT_EQ(f.v_[0], 0.5);
+
+    // x = 2 / x
+    f = div_scalar2(f);
+    EXPECT_NEAR(f.a_, 0.4, 1e-7);
+    EXPECT_EQ(f.v_[0], - TypeParam(2)/25 * 0.5);
+
+    // x /= 2
+    f = div_scalar3(f);
+    EXPECT_NEAR(f.a_, 0.2, 1e-7);
+    EXPECT_EQ(f.v_[0], - TypeParam(2)/25 * 0.5 * 0.5);
+}
+
+TYPED_TEST(JetTest, jet_mul_div_add_test)
+{
+    TypeParam x = 1;
+    TypeParam y = 2;
+    Jet<TypeParam, 2> a(x, 0);
+    Jet<TypeParam, 2> b(y, 1);
+    auto z = jet_mul_div_add(a, b);
+
+    EXPECT_NEAR(z.a_, jet_mul_div_add(x, y), 1e-6);
+    TypeParam dz_dx;
+    TypeParam dz_dy;
+
+    dz_dx = (y*y - x*x*y)/((x*x+y)*(x*x+y));
+    dz_dy = x*x*x/((x*x+y)*(x*x+y));
+    EXPECT_NEAR(z.v_[0], dz_dx, 1e-7);
+    EXPECT_NEAR(z.v_[1], dz_dy, 1e-7);
+}
+
+TYPED_TEST(JetTest, jet_sub_div)
+{
+    TypeParam x = 1;
+    TypeParam y = 2;
+    Jet<TypeParam, 2> a(x, 0);
+    Jet<TypeParam, 2> b(y, 1);
+    auto z = jet_sub_div(a, b);
+
+    EXPECT_NEAR(z.a_, jet_sub_div(x, y), 1e-7);
+    TypeParam dz_dx;
+    TypeParam dz_dy;
+
+    dz_dx = 2*y/((x+y)*(x+y));
+    dz_dy = -2*x/((x+y)*(x+y));
+    EXPECT_NEAR(z.v_[0], dz_dx, 1e-7);
+    EXPECT_NEAR(z.v_[1], dz_dy, 1e-7);
 }
 
 }  // namespace cnn
